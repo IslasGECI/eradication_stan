ccbb77d7d78ee572d133c5f3779f11028c1a0a44
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-17 23:01:42 +0000

    ♻️  Refactor

--- tests/test_run_predictions.py
+++ tests/test_run_predictions.py
@@ -8,7 +8,7 @@ def test_run_predictions():
     output_path = "predictions.csv"
     gtt.if_exist_remove(output_path)
     run_predictions(
-        model_path="/tests/data/cat_eradication",
+        model_path="tests/data/cat_eradication_for_tests",
         data_path="tests/data/data.json",
         initial_parameters="tests/data/init.json",
         output_path=output_path,
