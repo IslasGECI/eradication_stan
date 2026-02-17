645309d2eaacf0d711a3b19b2c2a6d0a02efdeec
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-17 22:41:50 +0000

    🛑🧪 Fail tests

---
+++ tests/test_run_predictions.py
@@ -0,0 +1,16 @@
+from eradication_stan.run_predictions import run_predictions
+
+import geci_test_tools as gtt
+
+
+def test_run_predictions():
+
+    output_path = "predictions.csv"
+    gtt.if_exist_remove(output_path)
+    run_predictions(
+        model_path="/tests/data/cat_eradication",
+        data_path="tests/data/data.json",
+        initial_parameters="tests/data/init.json",
+        output_path=output_path,
+    )
+    gtt.assert_exist(output_path)
