00c633ee151505c8be0553cb8ffb71cc1d6800c9
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-17 22:35:09 +0000

    ✅ Pass tests

---
+++ eradication_stan/build_model.py
@@ -0,0 +1,13 @@
+import subprocess
+
+
+def build_model(stan_file):
+    stan_basename = stan_file.split(".")[0]
+    try:
+        result = subprocess.run(
+            ["make", f"/workdir/{stan_basename}"],
+            cwd="/opt/cmdstan",
+        )
+        return result.returncode
+    except:
+        return "Not compiled"
